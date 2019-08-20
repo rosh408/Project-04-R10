4a065b144607ef636164d9991b85b18d846ac808
Rosh <rosh_408@hotmail.com>
2019-08-20 16:54:40 -0700

    Set up App Component in App.js

---
+++ js/App.js
@@ -0,0 +1,21 @@
+import React, {Fragment, Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>R10</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
